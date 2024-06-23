049f3eb95cc10000f49d5d6fd1b6407c82b26f3e
Novikov Andrei <novikov1108@gmail.com>
2024-06-23 21:15:34 +1000

    last commit

--- src/Components/TaskModal.js
+++ src/Components/TaskModal.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import '../Styles/TaskModal.css';
+import pencilIcon from '../Images/SocialIcons/pencil.svg';
 
-const TaskModal = ({ task, onClose, onComplete}) => {
+const TaskModal = ({ task, onClose, onComplete }) => {
     const [answer, setAnswer] = useState('');
     const [submitted, setSubmitted] = useState(false);
-
 
     const handleSubmit = () => {
         console.log('Answer:', answer);
@@ -23,13 +23,17 @@ const TaskModal = ({ task, onClose, onComplete}) => {
                     </div>
                 ) : (
                     <>
-                        <h2>{task.title}</h2>
-                        <p>Задание: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <div className="task-header">
+                            <div className='container_pencil'><img src={pencilIcon} alt="Pencil Icon" className="pencil-icon" /></div>
+                            <h2>{task.title}</h2>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <p className='second'>Lorem ipsum dolor sit amet, consectetur <br />adipiscing elit, sed do eiusmod tempor incididunt<br /> ut labore et dolore magna aliqua?</p>
+                        <p className='answer_title'>Ваш ответ:</p>
                         <textarea
-                            placeholder="Введите ваш ответ здесь..."
                             value={answer}
                             onChange={(e) => setAnswer(e.target.value)}
-                        ></textarea>
+                        />
                         <div className='modal-actions'>
                             <button onClick={handleSubmit}>Отправить</button>
                         </div>
